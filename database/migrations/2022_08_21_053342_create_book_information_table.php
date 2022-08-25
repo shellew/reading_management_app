8d0369ca88a6db8f6bc83dc578596cff48dbd496
shellew <nishioka2013_1124@yahoo.co.jp>
2022-08-25 12:04:02 +0900

    make book_information_table

---
+++ database/migrations/2022_08_21_053342_create_book_information_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_information', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->string('author');
+            $table->integer('isbn');
+            $table->timestamp('register_date');
+            $table->string('memo',256)->comment('memo');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_information');
+    }
+};
