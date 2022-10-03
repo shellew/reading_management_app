e7496f55824394afdbab3619056beeb1a4d0b8de
shellew <nishioka2013_1124@yahoo.co.jp>
2022-10-03 14:13:06 +0900

    createBook関数修正

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -31,41 +31,19 @@ class ApiController extends Controller
       }
 
       // 変更後
-      // 空のデータがあってもデータを作成する
-      // public function createBook(Request $request) {
-      //   $book_master = new BookMaster;
-      //   $book_master->user_id = is_null($request->user_id) ? $book_master->user_id : $request->user_id;
-      //   $book_master->title = is_null($request->title) ? $book_master->title : $request->title;
-      //   $book_master->author = is_null($request->author) ? $book_master->author : $request->author;
-      //   $book_master->isbn = is_null($request->isbn) ? $book_master->isbn : $request->isbn;
-      //   $book_master->register_date = is_null($request->register_date) ? $book_master->register_date : $request->register_date;
-      //   $book_master->memo = is_null($request->memo) ? $book_master->memo : $request->memo;
-      //   $book_master->status = is_null($request->status) ? $book_master->status : $request->status;
-      //   $book_master->save();
-
-      //   return response()->json([
-      //       "message" => "book record created"
-      //   ], 201);
-      // }
-    
-      // 変更前
       public function createBook(Request $request) {
-        // $user_id = "user_id";  //変更①
-        $book_master = BookMaster::find($user_id);
-        $cb = Carbon::now();
+        $cb = Carbon::today();
         $book_master = new BookMaster;
-        // user_idをquery文字列から取得
-        $book_master->user_id = $request->query("user_id");  //変更① または $book_master->user_id = $request->query($user_id); 
+        $book_master->user_id = $request->user_id;    //←ログイン画面作成後変更予定
         $book_master->title = $request->title;
-        $book_master->author = is_null($request->author) ? $book_master->author : $request->author;
-        //今日のデータを取得
-        $book_master->register_date = $request->$cb;  //変更予定②
-        $book_master->memo = is_null($request->memo) ? $book_master->memo : $request->memo;
+        $book_master->author = $request->has('author') ? $request->author : $request->input('author');
         $book_master->status = $request->status;
+        $book_master->memo = $request->has('memo') ? $request->memo : $request->input('memo');
+        $book_master->register_date = $cb->format('Y年m月d日');
         $book_master->save();
 
         return response()->json([
-            "message" => "book record created"
+          "message" => "book recored created"
         ], 201);
       }
     
@@ -91,18 +69,25 @@ class ApiController extends Controller
           // ②リクエストを受け取ると、user_id、title、author、isbn、register_date、memo、statusがnullかどうかチェックする。
           // nullの場合は、データベースのレコードを既存の値に置き換える。nullでない場合はnullが新い値として渡される。
 
-          
+      
+
+      //変更後
       public function updateBook(Request $request, $id) {
-        if(BookMaster::where('id', $id)->exists()) {
-          $book_master = BookMaster::find($id);
-          $book_master->user_id = is_null($request->user_id) ? $book_master->user_id : $request->user_id;
-          $book_master->title = is_null($request->title) ? $book_master->title : $request->title;
-          $book_master->author = is_null($request->author) ? $book_master->author : $request->author;
-          $book_master->isbn = is_null($request->isbn) ? $book_master->isbn : $request->isbn;
-          $book_master->register_date = is_null($request->register_date) ? $book_master->register_date : $request->register_date;
-          $book_master->memo = is_null($request->memo) ? $book_master->memo : $request->memo;
-          $book_master->status = is_null($request->status) ? $book_master->status : $request->status;
-          $book_master->save();
+        // if(BookMaster::where('id', $id)->exists()) {
+          // $book_master = BookMaster::find($id);
+          // $book_master->title = is_null($request->title) ? $book_master->title : $request->title;
+          // $book_master->author = is_null($request->author) ? $book_master->author : $request->author;
+          // $book_master->status = is_null($request->status) ? $book_master->title : $request->status;
+          // $book_master->title = is_null($request->memo) ? $book_master->title : $request->memo;
+          // $book_master->save();
+
+          if(BookMaster::where('id', $id)->exists()) {
+            $book_master = BookMaster::find($id);
+            $book_master->title = $request->title;
+            $book_master->author = $request->author;
+            $book_master->status = $request->status;
+            $book_master->title = $request->memo;
+            $book_master->save();
 
           return response()->json([
             "message" => "records updated successfully"
@@ -113,6 +98,30 @@ class ApiController extends Controller
           ], 404);
         }
       }
+
+        
+      //変更前
+      // public function updateBook(Request $request, $id) {
+      //   $id = 16;
+      //   if(BookMaster::where('id', $id)->exists()) {
+      //     $book_master = BookMaster::find($id);
+      //     // $book_master->user_id = is_null($request->user_id) ? $book_master->user_id : $request->user_id;
+      //     $book_master->title = is_null($request->title) ? $book_master->title : $request->title;
+      //     $book_master->author = is_null($request->author) ? $book_master->author : $request->author;
+      //     // $book_master->register_date = is_null($request->register_date) ? $book_master->register_date : $request->register_date;
+      //     $book_master->memo = is_null($request->memo) ? $book_master->memo : $request->memo;
+      //     $book_master->status = is_null($request->status) ? $book_master->status : $request->status;
+      //     $book_master->save();
+
+      //     return response()->json([
+      //       "message" => "records updated successfully"
+      //     ], 200);
+      //   } else {
+      //     return response()->json([
+      //       "message" => "Book not found"
+      //     ], 404);
+      //   }
+      // }
     
       public function deleteBook($id) {
         if(BookMaster::where('id', $id)->exists()) {
